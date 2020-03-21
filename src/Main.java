04c1e98a401fb1d535d8e4c77cda19c56d4a6e1a
lada <nicklada94@gmail.com>
2020-03-22 01:49:11 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int current = 2000000000;
+        int add = 500000000;
+        int total = current + add;
+        System.out.println(total);
+    }
+}
